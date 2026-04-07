aa7f193a6bc6d119e6d6c7088f0a4bcedc0d2d78
Isneha Manandhar <isnehamanandhar88@gmail.com>
2026-04-07 12:33:51 +0545

    converting home page to welcom page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,12 +1,29 @@
 import { Head, Link } from '@inertiajs/react';
-import Home from './Home';
+
 import ScrollToTop from '@/Components/ScrollToTop';
+import Hero from '@/M&M/Hero';
+import SecurityAbout from '@/M&M/Securityabout';
+import Products from '@/M&M/Products';
+import WhyChooseUs from '@/M&M/WhyChooseUs';
+import Services from '@/M&M/Services';
+import Project from '@/M&M/Project';
+import ContactBanner from '@/M&M/ContactBanner';
+import Testimonials from '@/M&M/Testimonials';
+import FAQ from '@/M&M/FAQ';
 
 export default function Welcome({}) {
   return (
         <>
             <Head title="Welcome" />
-           <Home/>
+         <Hero/>
+   <SecurityAbout/>
+   <Products/>
+   <WhyChooseUs/>
+   <Services/>
+    <Project/>
+    <ContactBanner/>
+    <Testimonials/>
+    <FAQ/>
         
            <ScrollToTop/>
         </>
